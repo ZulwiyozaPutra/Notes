ec02045c63ab9bca9b32354fa28fe932e4180976
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-06-09 01:46:04 -0700

    Remove and ignore .xcuserstate and .DS_Store files.

---
+++ notes/NotebookTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NotebookTableViewController.swift
+//  notes
+//
+//  Created by Zulwiyoza Putra on 6/9/17.
+//  Copyright © 2017 Zulwiyoza Putra. All rights reserved.
+//
+
+import Foundation
