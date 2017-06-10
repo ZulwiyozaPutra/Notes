8807c43face873ec3dfd5a13c9de462c3ed3dc57
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-06-09 22:23:27 -0700

    autosave now is available

--- notes/AppDelegate.swift
+++ notes/AppDelegate.swift
@@ -72,6 +72,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         preloadData()
+        stack.autoSave(60)
         return true
     }
 
